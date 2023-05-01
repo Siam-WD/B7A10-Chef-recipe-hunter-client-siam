183601f67fd4ea5283c54f74cc24f2e6e0d14084
Siam-WD <abusayedalsiam2023@gmail.com>
2023-05-01 23:50:09 +0600

    login and register page design

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,28 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import Main from './Components/Layout/Main.jsx';
+import Home from './Components/Pages/Home/Home.jsx';
+import Login from './Components/Pages/Login/Login.jsx';
+import Register from './Components/Pages/Register/Register.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      }
+    ]
   },
 ]);
 
